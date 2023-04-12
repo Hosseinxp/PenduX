918c668ce68b3fdfc26986df742b0d1b6965adde
hakimblr92 <125276226+hakimblr92@users.noreply.github.com>
2023-04-12 12:57:25 +0200

    Modified

--- OneDrive/Desktop/codes/learning/darsman/PenduX/app/src/main/java/com/learning/penxu_x/MainActivity.java
+++ OneDrive/Desktop/codes/learning/darsman/PenduX/app/src/main/java/com/learning/penxu_x/MainActivity.java
@@ -3,20 +3,39 @@ package com.learning.penxu_x;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_pageMain_Jouer,btn_pageMain_Setting;
+    private MediaPlayer mediaPlayer;
+    private static boolean run_music;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SetupView();
 
-        BtnSetup();
+//        setRun_music(true);
+        if(getRun_music()) {
+            mediaPlayer.start();
+        }else{
+            mediaPlayer.stop();
+        }
 
+        SettingButton();
+
+    }
+
+    // Création des buttons 'Jouer' et 'paramétrage'
+    public void SetupView(){
+        this.mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.smoke);
+        btn_pageMain_Jouer=findViewById(R.id.btn_pageMain_Jouer);
+        btn_pageMain_Setting=findViewById(R.id.btn_pageMain_Setting);
         btn_pageMain_Jouer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void SettingButton(){
         btn_pageMain_Setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,9 +56,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Création des buttons 'Jouer' et 'paramétrage'
-    public void BtnSetup(){
-        btn_pageMain_Jouer=findViewById(R.id.btn_pageMain_Jouer);
-        btn_pageMain_Setting=findViewById(R.id.btn_pageMain_Setting);
+
+    public boolean getRun_music() {
+        return run_music;
+    }
+
+    public void setRun_music(boolean run_music) {
+        this.run_music = run_music;
     }
 }
